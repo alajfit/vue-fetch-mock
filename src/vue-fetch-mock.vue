300cfe8eadea847c16073df2c633715124b4cfea
Alan Fitzpatrick <alajfit@gmail.com>
2020-01-31 23:25:21 +0000

    Created base of the component

--- src/vue-fetch-mock.vue
+++ src/vue-fetch-mock.vue
@@ -1,5 +1,77 @@
 <template>
     <div>
-        
+        <slot>Issues Mounting Component</slot>
     </div>
 </template>
+
+<script>
+export default {
+    name: 'VueFetchMock',
+    props: {
+        logger: {
+            type: Boolean,
+            default: false
+        },
+        throttle: {
+            type: Number,
+            default: 300
+        },
+        mocks: {
+            type: Array,
+            default: []
+        }
+    },
+    beforeMount() {
+        this.mock()
+    },
+    beforeDestroy() {
+        if (fetchMock.__prevProxy === this) this.unmock()
+    },
+    methods: {
+        mock() {
+            this.unmock()
+            const mocks = this.mocks
+            if (mocks) {
+                mocks.forEach(mock => {
+                    fetchMock.mock({
+                        ...mock,
+                        response: (url, opts) => {
+                            if (this.logger) console.info('fetch', url, opts)
+                            let result = {
+                                body: mock.response,
+                                headers: new Headers({
+                                    'content-type': 'application/json',
+                                }),
+                            }
+
+                            if (
+                                mock.response.hasOwnProperty('body') ||
+                                mock.response.hasOwnProperty('status') ||
+                                mock.response.hasOwnProperty('headers')
+                            ) {
+                                result = {
+                                    ...result,
+                                    ...mock.response,
+                                }
+                            }
+                            
+                            return this.throttle
+                                ? new Promise(resolve => { setTimeout(() => resolve(result), this.throttle) })
+                                : result
+                        }
+                    })
+                })
+
+                fetchMock.catch((...args) => fetchMock.realFetch.apply(window, args))
+                fetchMock.__prevProxy = this
+            }
+        },
+        unmock() {
+            if (typeof fetchMock.restore === 'function') {
+                fetchMock.restore()
+                delete fetchMock.__prevProxy
+            }
+        }
+    }
+}
+</script>
